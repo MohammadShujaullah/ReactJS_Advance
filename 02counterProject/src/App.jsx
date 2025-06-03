2eaa764e5ad96ce992d5276eda200eb1629cdba6
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-06-03 19:40:09 +0530

    Made a counter proejct ,just to learn about"vertual DOM"

---
+++ 02counterProject/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)   // "useState()"gives an array , in which at idx0, a counter is present and at idx1 a 
+  // function related to counter is present 
+  // it help   to synch. the UI 's things
+
+
+  function addValue() {
+    console.log("value added", counter)
+    if (counter < 20) { counter = counter + 1; }
+    setCounter(counter)
+
+  }
+  function removeValue() {
+    if (counter > 0) { setCounter(counter - 1) }
+  }
+
+  return (
+    <>
+      <h1>Chai or React</h1>
+      <h2>counter value:{counter}</h2>
+      <button onClick={addValue}>Add value:{counter}</button> <br/>
+      
+      <button onClick={removeValue}>remove value:{counter}</button>
+    </>
+  )
+}
+
+export default App
