67d69b0eeaa5b23aa399b47cbd6d3f98352ec686
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-06-03 19:43:54 +0530

    Creating my React Project through traditional method, which is time consuming

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./Chai";
+
+function App() {
+  return (          // we can only return one component at 
+                            // atime , if you want to return multiple,
+                            //  then you have put it in a dev
+    <div>
+      <h1>Hello bachchon!!</h1>
+      <Chai />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+
+function Chai(){   // chai is componet so , it always start with the capital letter 
+    return(
+     <h1>i am ironman</h1>
+
+    )
+}
+
+export default Chai
