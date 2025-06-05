9f04f67d7ed687e833ef4a2cf64fddf0c57cf01e
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-06-05 19:15:02 +0530

    added some extra code which is important for interview point of view, as i added duplicate line , then what happens with the output at webpage

--- 02counterProject/src/App.jsx
+++ 02counterProject/src/App.jsx
@@ -13,18 +13,27 @@ function App() {
     console.log("value added", counter)
     if (counter < 20) { counter = counter + 1; }
     setCounter(counter)
-
   }
   function removeValue() {
-    if (counter > 0) { setCounter(counter - 1) }
+    if (counter > 0) {
+      setCounter(counter - 1)
+      setCounter(counter - 1)
+      setCounter(counter - 1)    
+      setCounter(counter - 1)
+        // adding these lines , doesnt change the output, because,
+      //     here the whole work is goes at same time 
+      // and if you want to do decreament directly 4 time , then do
+      //"setCounter(prevcounter=>prevcounter-1)"
+
+    }
   }
 
   return (
     <>
       <h1>Chai or React</h1>
       <h2>counter value:{counter}</h2>
-      <button onClick={addValue}>Add value:{counter}</button> <br/>
-      
+      <button onClick={addValue}>Add value:{counter}</button> <br />
+
       <button onClick={removeValue}>remove value:{counter}</button>
     </>
   )
